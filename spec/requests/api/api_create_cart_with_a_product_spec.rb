bc696a35fbd86ad4c1a1d6247783d8e58fdb7450
shagufta <shagufta.batool8@gmail.com>
2022-02-21 02:30:20 +0100

    add factoryBots for cart and user models

--- spec/requests/api/api_create_cart_with_a_product_spec.rb
+++ spec/requests/api/api_create_cart_with_a_product_spec.rb
@@ -1,34 +1,30 @@
-RSpec.describe 'POST /api/orders', type: :request do
+RSpec.describe 'POST /api/carts', type: :request do
   let!(:user) { create(:user) }
-  let(:product) { create(:product) }
+  let!(:products) { create(:product) }
   subject { response }
   before do
-    post '/api/orders', params: {
-      order:
-       { product_id: product.id, user_id: user.id }
-    }
-    @order = Order.last
+    post '/api/carts'
   end
 
   it { is_expected.to have_http_status :created }
 
-  it 'is expected to create an instance of Order' do
-    expect(@order).to_not eq nil
+  it 'is expected to create an instance of Cart' do
+    expect(@cart).to_not eq nil
   end
 
   it 'is expected to associate order with user' do
     expect(@order.user).to eq user
   end
 
-  it 'is expected to create an order_item' do
-    expect(@order.items).to_not eq nil
+  it 'is expected to create an cart_item' do
+    expect(@cart.items).to_not eq nil
   end
 
-  it 'is expected to associate the product with order' do
-    expect(@order.products.first).to eq product
+  it 'is expected to associate the product with cart' do
+    expect(@cart.products.first).to eq product
   end
 
-  it 'is expected to include order_id in the response body' do
-    expect(response_json['order']['id']).to eq @order.id
+  it 'is expected to include cart_id in the response body' do
+    expect(response_json['cart']['id']).to eq @cart.id
   end
 end
